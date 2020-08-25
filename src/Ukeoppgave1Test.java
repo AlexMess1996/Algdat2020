cdc1f4c35d9560fd694669ef42c608a40e1e440d
AlexMess1996 <60467614+AlexMess1996@users.noreply.github.com>
2020-08-25 21:01:03 +0200

    Added Test for Ukeoppgave1 good example of testing

--- src/Ukeoppgave1Test.java
+++ src/Ukeoppgave1Test.java
@@ -10,5 +10,34 @@ class Ukeoppgave1Test {
 
         assertEquals(6, minimum);
         assertEquals(1,a[minimum]);
+
+        //test for alle permutasjoner for 4 tall.
+        {
+            int[][] a4 = {
+                    {1,2,3,4},{1,2,4,3},{1,3,2,4},{1,3,4,2},{1,4,2,3},{1,4,3,2},
+                    {2,1,3,4},{2,1,4,3},{2,3,1,4},{2,3,3,1},{2,4,1,3},{2,4,3,1},
+                    {3,2,1,4},{3,2,4,1},{3,1,2,4},{3,1,4,2},{3,4,2,1},{3,4,1,2},
+                    {4,2,3,1},{4,2,1,3},{4,3,2,1},{4,3,1,2},{4,1,2,3},{4,1,3,2}
+            };
+            int expected[] = {  //index hvor det finnes det minste tallet.
+                    0,0,0,0,0,0,
+                    1,1,2,3,2,3,
+                    2,3,1,1,3,2,
+                    3,2,3,2,1,1
+            };
+
+            for(int i=0; i<a4.length; ++i){
+                assertEquals(expected[i],Ukeoppgave1.min(a4[i]));
+            }
+        }
+
+        //test corner cases = "Spesialtilfeller"
+
+        int[] b = {1,4,17,10,6,20,8};
+        assertEquals(0,Ukeoppgave1.min(b));
+
+        int[] c = {8,4,17,10,6,20,1};
+        assertEquals(6,Ukeoppgave1.min(c));
+
     }
 }
