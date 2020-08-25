6da1630b716acb10cc18ef2ac9e393b43d2c23f7
AlexMess1996 <60467614+AlexMess1996@users.noreply.github.com>
2020-08-25 20:04:15 +0200

    First version with skeleton code

---
+++ src/Ukeoppgave1.java
@@ -0,0 +1,18 @@
+public class Ukeoppgave1 {
+    public static void main (String [] args){
+
+        int[] a = {8,4,17,10,6,20,1,11,15,3,18,9,2,7,19};
+
+        int min_index = min(a);
+
+        System.out.println(min_index);
+    }
+
+    static int min(int[] a){
+        int min_value = a[0];
+        int min_index = 0;
+
+        return min_index;
+    }
+}
+
