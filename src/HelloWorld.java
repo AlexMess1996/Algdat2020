aeafdfdf63e2d7d20bbb535bd345273799b58b7d
AlexMess1996 <60467614+AlexMess1996@users.noreply.github.com>
2020-08-25 19:30:37 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        for (int i=1; i<=10; ++i){
+            System.out.println(i);
+        }
+    }
+}
